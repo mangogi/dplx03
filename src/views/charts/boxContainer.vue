8b8adc91cc24d55ac4621b2f5a252689fe3022b5
xiongj <1056394575@qq.com>
2022-03-16 19:32:58 +0800

    feat: 新增listBox和boxContainner组件

---
+++ src/views/charts/boxContainer.vue
@@ -0,0 +1,147 @@
+<template>
+  <div
+    class="box_container"
+    :style="{ height: boxHeight + 'px', width: boxWidth + 'px' }"
+  >
+    <div class="box_title" :style="{ width: titleWidth + 'px' }">
+      <div class="box_title_flex">
+        <span>{{ title }}</span>
+        <select
+          class="provice_select"
+          name="province-choice"
+          v-model="couponSelected"
+          @change="getCouponSelected"
+          v-show="showSelect"
+        >
+          <option
+            v-for="(coupon, index) in optionData"
+            :key="index"
+            :value="index"
+          >
+            {{ coupon }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'boxContainer',
+  props: {
+    title: {
+      type: String,
+      default: '各险种缴费分布',
+    },
+    optionData: {
+      type: String,
+      default: () => {
+        return ['征缴总金额', '全部']
+      },
+    },
+    showSelect: {
+      type: Boolean,
+      default: true,
+    },
+    boxWidth: {
+      type: String,
+      default: '420',
+    },
+    boxHeight: {
+      type: String,
+      default: '489',
+    },
+    titleWidth: {
+      type: String,
+      default: '395',
+    },
+  },
+  data() {
+    return {
+      couponSelected: 0,
+    }
+  },
+  created() {
+    this.couponSelected = 0
+  },
+  methods: {
+    getCouponSelected() {},
+  },
+}
+</script>
+
+<style lang="less">
+.bgcorner {
+  // background:bg-color bg-image position/bg-size bg-repeat bg-origin bg-clip bg-attachment initial|inherit;;
+  background: linear-gradient(to top, #2383ed, #2383ed) left top no-repeat,
+    // 上左
+      linear-gradient(to right, #2383ed, #2383ed) left top no-repeat,
+    //坐上
+      linear-gradient(to left, #2383ed, #2383ed) right top no-repeat,
+    //上右
+      linear-gradient(to bottom, #2383ed, #2383ed) right top no-repeat,
+    //上右
+      linear-gradient(to left, #2383ed, #2383ed) left bottom no-repeat,
+    // 下左
+      linear-gradient(to bottom, #2383ed, #2383ed) left bottom no-repeat,
+    // 左下
+      linear-gradient(to top, #2383ed, #2383ed) right bottom no-repeat,
+    // 下右
+      linear-gradient(to left, #2383ed, #2383ed) right bottom no-repeat; //右下
+
+  background-size: 3px 16px, 16px 3px, 3px 16px, 16px 3px;
+}
+.box_container {
+  margin-left: 20px;
+  background-color: rgba(7, 16, 24, 0.4);
+  border: solid 1px #2564ae;
+  .bgcorner();
+  .box_title {
+    width: 395px;
+    height: 40px;
+    background-image: linear-gradient(
+      -90deg,
+      rgba(7, 40, 84, 0.4),
+      rgba(54, 152, 255, 0.2)
+    );
+    background-blend-mode: normal, normal;
+    margin-left: 9px;
+    margin-top: 9px;
+    color: #fefefe;
+    font-size: 18px;
+    // display: flex;
+    // flex-direction: row;
+    // justify-content: space-between;
+    .box_title_flex {
+      width: 95%;
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-top: -33px;
+      margin-left: 15px;
+      .provice_select {
+        background-color: #061524;
+        border: solid 1px #134086;
+        border: 0px;
+        font-size: 18px;
+        height: 33px;
+        color: #ffffff;
+        font-size: 16px;
+        // margin-top: 4px;
+      }
+    }
+  }
+  .box_title::before {
+    content: '1';
+    line-height: 40px;
+    width: 6px;
+    font-size: 28px;
+    height: 40px;
+    color: #2383ed;
+    background-color: #2383ed;
+    border-right: 2px solid #04090e;
+  }
+}
+</style>
--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,17 +1,261 @@
 <template>
   <screen-adapter>
     <div class="main-page">
-      mainpage///
+      <!-- 标题部分 -->
+      <div class="main-title">
+        <div class="top_box">
+          <input
+            class="date_picker"
+            type="date"
+            name="start_time"
+            value="2020-2-3"
+            v-model="selectDay"
+            @change="getDay"
+          />
+          <div class="title_text">社保缴费分析</div>
+          <div class="day_time">{{ todyTime }}</div>
+        </div>
+      </div>
+      <!-- 七个列表部分 -->
+      <div class="list_box">
+        <list-box
+          :imgUrl="'01_70'"
+          :title="listData[0].title"
+          :val="listData[0].val"
+          :percent="listData[0].percent"
+        ></list-box>
+        <list-box
+          :imgUrl="'01_38'"
+          :title="listData[1].title"
+          :val="listData[1].val"
+          :percent="listData[1].percent"
+        ></list-box>
+        <list-box
+          :imgUrl="'01_7'"
+          :title="listData[2].title"
+          :val="listData[2].val"
+          :percent="listData[2].percent"
+        ></list-box>
+        <list-box
+          :imgUrl="'01_75'"
+          :title="listData[3].title"
+          :val="listData[3].val"
+          :percent="listData[3].percent"
+        ></list-box>
+        <list-box
+          :imgUrl="'01_44'"
+          :title="listData[4].title"
+          :val="listData[4].val"
+          :unit="listData[4].unit"
+          :percent="listData[4].percent"
+        ></list-box>
+        <list-box
+          :imgUrl="'01_62'"
+          :title="listData[5].title"
+          :val="listData[5].val"
+          :percent="listData[5].percent"
+        ></list-box>
+        <list-box
+          :imgUrl="'01'"
+          :title="listData[6].title"
+          :val="listData[6].val"
+          :percent="listData[6].percent"
+        ></list-box>
+      </div>
+      <!-- 中间三个图 -->
+      <div class="center_box">
+        <!-- 缴费分布柄图 -->
+        <box-container class="pie_charts" :boxWidth="'420'"></box-container>
+        <box-container
+          :boxWidth="'960'"
+          :titleWidth="'940'"
+          :showSelect="false"
+          :title="'各险种缴费明细'"
+        ></box-container>
+        <box-container
+          :title="'各险种补缴/退缴情况'"
+          :showSelect="false"
+          :boxWidth="'420'"
+        ></box-container>
+      </div>
+      <div class="bottom_box">
+        <box-container
+          class="pie_charts"
+          :boxWidth="'600'"
+          :titleWidth="'574'"
+          :boxHeight="'320'"
+          :title="'缴费趋势分析'"
+        ></box-container>
+        <box-container
+          class="pie_charts"
+          :boxWidth="'600'"
+          :titleWidth="'574'"
+          :boxHeight="'320'"
+          :title="'缴费趋势分析'"
+        ></box-container>
+        <box-container
+          class="pie_charts"
+          :boxWidth="'600'"
+          :titleWidth="'574'"
+          :boxHeight="'320'"
+          :title="'缴费趋势分析'"
+        ></box-container>
+      </div>
     </div>
   </screen-adapter>
 </template>
 <script>
 import screenAdapter from './charts/ScreenAdapter.vue'
+import listBox from './charts/listBox.vue'
+import boxContainer from './charts/boxContainer.vue'
 export default {
   name: 'mainPage',
   components: {
     screenAdapter,
+    listBox,
+    boxContainer,
+  },
+  data() {
+    return {
+      selectDay: '', // 日期选择器
+      todyTime: '', // 当前日期
+      listData: [
+        { title: '个人缴费金额', unit: '万元', percent: '+2.3%', val: '34.89' },
+        { title: '单位缴费金额', unit: '万元', percent: '+2.3%', val: '86.89' },
+        { title: '征缴总金额', unit: '万元', percent: '+2.3%', val: '987.8' },
+        { title: '缴费总人数', unit: '万元', percent: '-2.3%', val: '34.89' },
+        { title: '征缴率', unit: '%', percent: '+2.3%', val: '96' },
+        { title: '补缴金额', unit: '万元', percent: '+2.3%', val: '234.9' },
+        { title: '退费金额', unit: '万元', percent: '-2.3%', val: '56.89' },
+      ],
+    }
+  },
+  mounted() {
+    this.getNowTime()
+  },
+  methods: {
+    getDay() {},
+    getNowTime() {
+      let startDate = new Date()
+      let data = startDate
+        .toLocaleDateString()
+        .split('/')
+        .join('-')
+      let hours = startDate.getHours()
+      let mins = startDate.getMinutes()
+      this.todyTime = data.concat(' ' + hours, ':', mins)
+      console.log(this.todyTime)
+    },
   },
 }
 </script>
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.bgcorner {
+  // background:bg-color bg-image position/bg-size bg-repeat bg-origin bg-clip bg-attachment initial|inherit;;
+  background: linear-gradient(to top, #2383ed, #2383ed) left top no-repeat,
+    // 上左
+      linear-gradient(to right, #2383ed, #2383ed) left top no-repeat,
+    //坐上
+      linear-gradient(to left, #2383ed, #2383ed) right top no-repeat,
+    //上右
+      linear-gradient(to bottom, #2383ed, #2383ed) right top no-repeat,
+    //上右
+      linear-gradient(to left, #2383ed, #2383ed) left bottom no-repeat,
+    // 下左
+      linear-gradient(to bottom, #2383ed, #2383ed) left bottom no-repeat,
+    // 左下
+      linear-gradient(to top, #2383ed, #2383ed) right bottom no-repeat,
+    // 下右
+      linear-gradient(to left, #2383ed, #2383ed) right bottom no-repeat; //右下
+
+  background-size: 3px 16px, 16px 3px, 3px 16px, 16px 3px;
+}
+.main-page {
+  width: 100%;
+  height: 100%;
+  background-color: #04090e;
+  display: flex;
+  flex-direction: column;
+  .main-title {
+    width: 99%;
+    height: 60px;
+    background: url('../assets/imgs/bg.png') no-repeat;
+    margin-left: 0.5%;
+    .top_box {
+      width: 95%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      margin-left: 2.5%;
+      .date_picker {
+        background-color: #061524;
+        color: #ffffff;
+        border: solid 1px #134086;
+        font-size: 18px;
+        height: 33px;
+        margin-left: 0px;
+        margin-top: 15px;
+      }
+      .title_text {
+        z-index: 22;
+        width: 200px;
+        height: auto;
+        font-family: SourceHanSansCN-Bold;
+        font-size: 28px;
+        line-height: 30px;
+        font-weight: bold;
+        font-stretch: normal;
+        letter-spacing: 1px;
+        color: #ffffff;
+        background: linear-gradient(to bottom, white, #44a5eb);
+        -webkit-background-clip: text;
+        color: transparent;
+        margin-top: 14px;
+      }
+      .day_time {
+        width: 149px;
+        height: 13px;
+        font-family: PingFang-SC-Bold;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        line-height: 40px;
+        letter-spacing: 0px;
+        color: #1b67de;
+        margin-top: 15px;
+      }
+    }
+  }
+  .list_box {
+    width: 1840px;
+    height: 100px;
+    background-color: rgba(7, 16, 24, 0.4);
+    border: solid 1px #2564ae;
+    margin-top: 30px;
+    margin-left: 40px;
+    display: flex;
+    flex-direction: row;
+    .bgcorner();
+  }
+  .center_box {
+    width: 1840px;
+    height: 489px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    .pie_charts {
+      margin-left: 40px;
+    }
+  }
+  .bottom_box {
+    width: 1840px;
+    height: 320px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: row;
+    .pie_charts {
+      margin-left: 40px;
+    }
+  }
+}
+</style>
