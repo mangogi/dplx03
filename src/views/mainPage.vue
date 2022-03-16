f77ad2a654e18b4cfefb528ca635fe5ff65cd94e
xiongj <1056394575@qq.com>
2022-03-16 12:30:40 +0800

    feat: 初始化项目

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    mainpage
-  </div>
+  <screen-adapter>
+    <div class="main-page">
+      mainpage
+    </div>
+  </screen-adapter>
 </template>
-
+<script>
+import screenAdapter from './charts/ScreenAdapter.vue'
+export default {
+  name: 'mainPage',
+  components: {
+    screenAdapter,
+  },
+}
+</script>
 <style scoped lang="less"></style>
